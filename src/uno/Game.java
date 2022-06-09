30c11c1b693363f84dda6a3547390315bd7893af
Dawson Vaught <vaught.dawson@gmail.com>
2022-06-09 15:34:40 -0700

    game logic

--- src/uno/Game.java
+++ src/uno/Game.java
@@ -8,33 +8,56 @@ public class Game {
 	public Card topCard;
 
 	public Game (int numOfPlayers) {
+		this.playerList = new ArrayList<Player>();
 		this.unoDeck = new Deck();
 		this.topCard = unoDeck.drawOne();
-		int i = 0;
-		while (i < numOfPlayers) {
+		for (int i = 0; i < numOfPlayers; i++) {
 			System.out.println("Player please enter your name."); // Would like to add player number
 			String name = System.console().readLine();
 			Player player = new Player(name);
 			player.setHand(unoDeck.drawCards(7));
 			playerList.add(player);
-
 		}
 	}
 
 	public void runGame() {
 		boolean isWinner = false;
 		int numOfPlayers = playerList.size();
-		int turnCounter = 0;
+		int turn = 0;
+		int turnMod = 1;
 		while (!isWinner) {
-			if(turnCounter < numOfPlayers) {
-				System.out.println(String.format("The card on top is %s %d", topCard.getColor(), topCard.getNumber()));
-				Player currentPlayer = playerList.get(turnCounter);
-				System.out.println(String.format("%s, it is your turn.", currentPlayer.getName()));
-				System.out.println(String.format("Your currently have these cards %s", currentHand(currentPlayer)));
-				System.out.println("The cards you can play are:");
-				System.out.println(playableCards(currentPlayer));
-			}
+				Player currentPlayer = playerList.get(turn);
+				
+				System.out.printf("\nThe card on top is %s\nIt is %s's turn\\n\\n", topCard.toString(), currentPlayer.getName());
+				
+				Card playedCard;
+				
+				if (currentPlayer.playable(topCard).size() == 0) {
+					System.out.printf("%s didn't have a card they could play...", currentPlayer.getName());
+					
+					ArrayList<Card> addedCards = new ArrayList<Card>();
+					boolean isValidCard = false;
+					
+					while(!isValidCard) {
+						Card drawedCard = this.unoDeck.drawOne();
+						isValidCard = drawedCard.canPlayOn(topCard);
+					}
+					
+					playedCard = addedCards.remove(addedCards.size()-1);
+					ArrayList<Card> newHand = new ArrayList<Card>();
+					newHand.addAll(addedCards);
+					newHand.addAll(currentPlayer.getHand());
+					currentPlayer.setHand(newHand);
+					
+					System.out.printf("%s drew %d cards!\n", currentPlayer.getName(), addedCards.size() + 1);
+				} else {
+					playedCard = currentPlayer.playCard(topCard);
+				}
+				
+				System.out.printf("%s played a %s\n", currentPlayer.getName(), playedCard.toString());
+				
 
+				turn = turn + turnMod % playerList.size();
 		}
 
 	}
